5c655f3aaa39047ab259e63ec90a22136e9a0055
F. T. Costa <47283591+CasualCosta@users.noreply.github.com>
2024-05-01 18:00:22 -0300

    Initial commit

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+print("Seja bem-vindo(ª) à calculadora de Índice de Massa Corporal!")
+while True:
+    try:
+        massa = float(input("Escreva sua massa em quilogramas: "))
+        break
+    except ValueError:
+        print("Valor inválido. Favor utilizar apenas números e, opcionalmente, um ponto.")
+while True:
+    try:
+        altura = float(input("Escreva sua altura em metros: "))
+        break
+    except ValueError:
+        print("Valor inválido. Favor utilizar apenas números e, opcionalmente, um ponto.")
+imc = float(massa) / float(float(altura) ** 2)
+mensagem = "Seu índice de massa corporal é " + str(imc) + "."
+print(mensagem)
